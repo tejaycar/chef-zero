de55194c0d61caa53757dd5804d524add3d1f877
Tejay Cardon <Tejay.cardon@gmail.com>
2014-03-18 21:35:39 -0600

    Add tests

---
+++ chef-zero-test/chefconfig/knife.rb
@@ -0,0 +1,7 @@
+log_level                :info
+log_location             STDOUT
+node_name                'dummy'
+client_key               '/chefconfig/dummy.pem'
+validation_client_name   'dummy'
+validation_key           '/home/vagrant/dummy'
+cookbook_path [ '/cookbooks' ]
